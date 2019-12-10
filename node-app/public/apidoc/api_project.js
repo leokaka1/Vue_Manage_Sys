c10a9d441c2592a6da12d6d7accc0bc831a74b7e
leokaka1 <leocaoxiaozhu@163.com>
2019-12-10 15:04:55 +0800

    添加了apidoc的相关文档

---
+++ node-app/public/apidoc/api_project.js
@@ -0,0 +1,1 @@
+define({  "title": "管理系统接口文档",  "url": "http://localhost:5000",  "name": "数字管理系统后台",  "version": "0.1.0",  "description": "数字管理系统后台",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-12-10T06:50:40.016Z",    "url": "http://apidocjs.com",    "version": "0.19.0"  }});
--- node-app/routers/api/users.js
+++ node-app/routers/api/users.js
@@ -2,7 +2,19 @@
 const express = require('express')
 const router = express.Router()
 
-
+/**
+ * @api {get} /api/users/test 用户测试
+ * @apiDescription 用户测试
+ * @apiName test
+ * @apiGroup Users
+ * @apiSuccess {json} msg
+ * @apiSuccessExample {json} Success-Response:
+ *  {
+ *      "msg":"返回成功"
+ *  }
+ * @apiSampleRequest http://localhost:5000/api/users/test
+ * @apiVersion 1.0.0
+ */
 router.get('/test',(req,res)=>{
     res.json({
         msg:'user login works'
