1427bab3973bedf7c7810f80be02751c40cf4c32
leokaka1 <leocaoxiaozhu@163.com>
2019-12-11 15:50:00 +0800

    添加了修改商品接口和删除商品接口

--- node-app/routers/api/Profits.js
+++ node-app/routers/api/Profits.js
@@ -11,15 +11,6 @@ const Profits = require('../../models/Profits')
  * @apiGroup Profits
  * @apiSuccess {string} code 状态码 0：失败 1：成功
  * @apiSuccess {string} msg 信息返回
- * @apiSuccess {json} data 返回数据
- * @apiSuccess {string} date 时间
- * @apiSuccess {string} _id 用户ID
- * @apiSuccess {string} type 货品类型
- * @apiSuccess {string} desc 描述
- * @apiSuccess {string} income 进账
- * @apiSuccess {string} expend 支出
- * @apiSuccess {string} cash 现金
- * @apiSuccess {string} remark 备注
  * @apiSuccessExample {json} Success-Response:
  *  {
     "code": 1,
@@ -52,10 +43,54 @@ router.post('/add',passport.authenticate('jwt',{session:false}),async(req,res)=>
     }
 })
 
+
+/**
+ * @api {post} /api/profits/edit/:id 修改货品信息
+ * @apiDescription 修改货品信息
+ * @apiName edit
+ * @apiGroup Profits
+ * @apiParam id 货品Id
+ * @apiSuccess {string} code 状态码 0：失败 1：成功
+ * @apiSuccess {string} msg 信息返回
+ * @apiSuccessExample {json} Success-Response:
+ *  {
+    "code": 1,
+    "msg":"商品修改成功"
+}
+ * @apiSampleRequest http://localhost:5000/api/profits/edit/:id
+ * @apiVersion 1.0.0
+ */
+router.post('/edit/:id',passport.authenticate('jwt',{session:false}),async(req,res)=>{
+    const profitsFields = {}
+    const {type,desc,income,expend,cash,remark} = req.body
+    if(type) profitsFields.type = type
+    if(type) profitsFields.desc = desc
+    if(type) profitsFields.income = income
+    if(type) profitsFields.expend = expend
+    if(type) profitsFields.cash = cash
+    if(type) profitsFields.remark = remark
+
+    const result = await Profits.findOneAndUpdate(
+        {_id:req.params.id},
+        {$set:profitsFields},
+        {new:true}
+    )
+    if(result){
+        res.send({
+            code:1,
+            msg:"商品修改成功"
+        })
+    }else{
+        res.send({
+            code:0,
+            msg:"商品修改失败"
+        })
+    }
+})
+
 /**
  * @api {post} /api/profits/ 查询所有物品信息
  * @apiDescription 查询所有信息
- * @apiName add
  * @apiGroup Profits
  * @apiSuccess {string} code 状态码 0：失败 1：成功
  * @apiSuccess {string} msg 提示信息
@@ -98,7 +133,6 @@ router.get('/',passport.authenticate('jwt',{session:false}), async (req,res)=>{
 /**
  * @api {post} /api/profits/:id 查询单个物品信息
  * @apiDescription 查询单个信息
- * @apiName add
  * @apiGroup Profits
  * @apiSuccess {string} code 状态码 0：失败 1：成功
  * @apiSuccess {string} msg 提示信息
@@ -120,7 +154,7 @@ router.get('/',passport.authenticate('jwt',{session:false}), async (req,res)=>{
         }
     ]
 }
- * @apiSampleRequest http://localhost:5000/api/profits/
+ * @apiSampleRequest http://localhost:5000/api/profits/:id
  * @apiVersion 1.0.0
  */
 router.get('/:id',passport.authenticate('jwt',{session:false}), async (req,res)=>{
@@ -138,5 +172,50 @@ router.get('/:id',passport.authenticate('jwt',{session:false}), async (req,res)=
         })
     }
 })
+
+/**
+ * @api {post} /api/profits/:id 查询单个物品信息
+ * @apiDescription 查询单个信息
+ * @apiGroup Profits
+ * @apiSuccess {string} code 状态码 0：失败 1：成功
+ * @apiSuccess {string} msg 提示信息
+ * @apiSuccess {json} data 数据
+ * @apiSuccessExample {json} Success-Response:
+ *  {
+    "code": 1,
+    "data": [
+        {
+            "date": "2019-12-11T05:08:52.868Z",
+            "_id": "5df079ec41a5ca68bee9ec9e",
+            "type": "优惠券",
+            "desc": "购买课程",
+            "income": "20",
+            "expend": "20",
+            "cash": "500",
+            "remark": "购买vue全栈课程",
+            "__v": 0
+        }
+    ]
+}
+ * @apiSampleRequest http://localhost:5000/api/profits/:id
+ * @apiVersion 1.0.0
+ */
+router.delete('/delete/:id',passport.authenticate('jwt',{session:false}), async (req,res)=>{
+    const result = await Profits.findOneAndRemove({_id:req.params.id})
+    
+    if(result){
+        res.json({
+            code:1,
+            msg:"商品删除成功"
+        })
+    }else{
+        res.json({
+            code:0,
+            msg:"商品删除失败"
+        })
+    }
+})
+
+
 
 module.exports = router
